d9d47e951b314ac64db0ca613d09daada8dbe6f1
AndreeaFrincu <frincu60@gmail.com>
2021-08-16 15:44:46 +0300

    Failed attempts at Products.vue filters.

--- src/store/products/store.js
+++ src/store/products/store.js
@@ -1,55 +1,19 @@
 import _ from "lodash";
+import {stat} from "copy-webpack-plugin/dist/utils/promisify";
 
 const state = {
-  productsList: []
+
 }
 const mutations = {
-  setProducts(state, value) {
-    state.productsList = value
-  }
+
 }
 const actions = {
   setProducts({commit}) {
     //retrieve data
-    let data = [
-      {
-        id: 1,
-        name: 'genre1aaaaaa'
-      },
-      {
-        id: 2,
-        name: 'genre2'
-      },
-      {
-        id: 3,
-        name: 'genre3'
-      },
-      {
-        id: 4,
-        name: 'genre4'
-      },
-      {
-        id: 5,
-        name: 'genre5'
-      },
-      {
-        id: 6,
-        name: 'genre6'
-      },
-      {
-        id: 7,
-        name: 'genre7'
-      }
-    ]
-    //transform data if necessary
-    //set data
-    commit('setProducts', data)
   }
 }
 const getters = {
-    getProducts: state => {
-      return state.productsList
-    }
+
 }
 
 export default {
