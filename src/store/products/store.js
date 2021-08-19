822f09f8c8b6740354f564c2d6721547b359ff07
AndreeaFrincu <frincu60@gmail.com>
2021-08-19 15:12:53 +0300

    Sort is working, trying to paginate

--- src/store/products/store.js
+++ src/store/products/store.js
@@ -13,9 +13,6 @@ const mutations = {
   setProducts(state, value) {
     state.productsList = value
   },
-  setProductsByAscPrice(state, value) {
-    state.productsList = value
-  },
   setGenres(state, value) {
     state.genresList = value
   },
@@ -38,50 +35,132 @@ const actions = {
     //set data
     commit('setProducts', products)
     commit('setGenres', genres)
+    commit('pagination', )
   }
 }
 const getters = {
   getProducts: state => {
+    /** Case: Products not filtered and not sorted by anything */
     if(state.selectedGenreList.length === 0 && state.sortBy === '' && state.onPage === ''){
       return state.productsList
     }
-    else if(state.sortBy === '+price'){
-      return state.productsList.sort( function( a , b){
-          if(a.price > b.price) return 1;
-          if(a.price < b.price) return -1;
-          return 0;
+    /** Case: Products filtered and not sorted by anything */
+    else if(state.selectedGenreList.length !== 0 && state.sortBy === ''){
+      let filteredProductsList = state.productsList.filter((product) => {
+        return product.genres.some((genre) => {
+          return state.selectedGenreList.some((currentSelectedGenre) => {
+            return currentSelectedGenre.id === genre.id
+          })
         })
+      })
+      return filteredProductsList
     }
-    else if(state.sortBy === '-price'){
-      return state.productsList.sort( function( a , b){
+    /** Case: Products filtered and sorted by price (ascending) */
+    else if(state.selectedGenreList.length !== 0 && state.sortBy === '+price'){
+      let filteredProductsList
+        filteredProductsList = state.productsList.filter((product) => {
+        return product.genres.some((genre) => {
+          return state.selectedGenreList.some((currentSelectedGenre) => {
+            return currentSelectedGenre.id === genre.id
+          })
+        })
+      })
+      return filteredProductsList.sort( function(a , b){
+        if(a.price > b.price) return 1;
+        if(a.price < b.price) return -1;
+        return 0;
+      })
+    }
+    /** Case: Products filtered and sorted by price (descending) */
+    else if(state.selectedGenreList.length !== 0 && state.sortBy === '-price'){
+      let filteredProductsList
+      filteredProductsList = state.productsList.filter((product) => {
+        return product.genres.some((genre) => {
+          return state.selectedGenreList.some((currentSelectedGenre) => {
+            return currentSelectedGenre.id === genre.id
+          })
+        })
+      });
+      return filteredProductsList.sort( function(a , b){
         if(a.price < b.price) return 1;
         if(a.price > b.price) return -1;
         return 0;
       })
     }
-    else if(state.sortBy === '+alpha'){
-      return state.productsList.sort( function( a , b){
+    /** Case: Products filtered and sorted alphabetical (ascending) */
+    else if(state.selectedGenreList.length !== 0 && state.sortBy === '+alpha'){
+      let filteredProductsList
+      filteredProductsList = state.productsList.filter((product) => {
+        return product.genres.some((genre) => {
+          return state.selectedGenreList.some((currentSelectedGenre) => {
+            return currentSelectedGenre.id === genre.id
+          })
+        })
+      });
+      return filteredProductsList.sort( function(a , b){
         if(a.title > b.title) return 1;
         if(a.title < b.title) return -1;
         return 0;
       })
     }
-    else if(state.sortBy === '-alpha'){
-      return state.productsList.sort( function( a , b){
+    /** Case: Products filtered and sorted alphabetical (descending) */
+    else if(state.selectedGenreList.length !== 0 && state.sortBy === '-alpha'){
+      let filteredProductsList
+      filteredProductsList = state.productsList.filter((product) => {
+        return product.genres.some((genre) => {
+          return state.selectedGenreList.some((currentSelectedGenre) => {
+            return currentSelectedGenre.id === genre.id
+          })
+        })
+      });
+      return filteredProductsList.sort( function(a , b){
         if(a.title < b.title) return 1;
         if(a.title > b.title) return -1;
         return 0;
       })
     }
-    // else if(state.selectedGenreList.length !== 0 && state.sortBy === '+price' || state.onPage === ''){
-    //   return state.productsList.filter((product) => {
-    //     return product.genres.some((genre) => {
-    //       return state.selectedGenreList.some((currentSelectedGenre) => {
-    //         return currentSelectedGenre.id === genre.id
-    //       })
-    //     })
-    //   })
-    //}
+    /** Case: Products not filtered and sorted by price (ascending) */
+    else if(state.selectedGenreList.length === 0 && state.sortBy === '+price'){
+      return state.productsList.sort( function(a , b){
+        if(a.price > b.price) return 1;
+        if(a.price < b.price) return -1;
+        return 0;
+      })
+    }
+    /** Case: Products not filtered and sorted by price (descending) */
+    else if(state.selectedGenreList.length === 0 && state.sortBy === '-price'){
+      return state.productsList.sort( function(a , b){
+        if(a.price < b.price) return 1;
+        if(a.price > b.price) return -1;
+        return 0;
+      })
+    }
+    /** Case: Products not filtered and sorted alphabetical (ascending) */
+    else if(state.selectedGenreList.length === 0 && state.sortBy === '+alpha'){
+      return state.productsList.sort( function(a , b){
+        if(a.title > b.title) return 1;
+        if(a.title < b.title) return -1;
+        return 0;
+      })
+    }
+    /** Case: Products not filtered and sorted alphabetical (descending) */
+    else if(state.selectedGenreList.length === 0 && state.sortBy === '-alpha'){
+      return state.productsList.sort( function(a , b){
+        if(a.title < b.title) return 1;
+        if(a.title > b.title) return -1;
+        return 0;
+      })
+    }
+
+    // function paginate(array, page_size, page_number) {
+    //   return array.slice((page_number - 1) * page_size, page_number * page_size);
+    // }
+    // console.log(paginate(products, 1, 1));
+
+    /** Case: Products not filtered, not sorted, but on the page x products */
+    else if(state.selectedGenreList.length === 0 && state.sortBy === '' && state.onPage !== 0){
+      return state.productsList.slice((2 - 1) * state.onPage, 2 * state.onPage)
+    }
   },
   getGenres: state => {
     return state.genresList
